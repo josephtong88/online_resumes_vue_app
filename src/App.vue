b048ab94216643da9c0e805c56f7c41ed20a5482
Jonathan-Miksis <jmiksis13@gmail.com>
2021-12-06 21:55:44 -0500

    add column for contact information, need further testing

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,69 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
+    <div class="container">
+      <div class="row">
+        <div class="col-3">
+          <div class="headers">
+            Jonathan Miksis
+          </div>
+        </div>
+        <div class="col-9">
+          2 of 2
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <div class="headers">
+          Contact
+          <hr>
+          </div>
+        </div>
+        <div class="col-9">
+          2 of 3
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <div class="headers">
+          <b>Address:</b>
+          <br>
+          </div>
+          200 Quarry Hills Drive
+          Quincy, MA 02169
+        </div>
+        <div class="col-9">
+          2 of 3
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <br>
+          <div class="headers">
+          <b>Phone:</b>
+          </div>
+          (603) 812-7478
+          <br>
+        </div>
+        <div class="col-9">
+          2 of 3
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-3">
+          <br>
+          <div class="headers">
+          <b>Email:</b>
+          </div>
+          jmiksis13@gmail.com
+          <br>
+        </div>
+        <div class="col-9">
+          2 of 3
+        </div>
+      </div>
+</div>
+
   </div>
 </template>
 
@@ -28,5 +91,11 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.headers {
+  font-family: sans-serif, Arial, Helvetica, sans-serif;
+  font-weight: 900;
+  font-size: 20px;
 }
 </style>
